4fcd732534a88a81e062b2ea8af5746289051bd8
Niv Sardi <xaiki@evilgiggle.com>
2012-08-09 14:07:52 -0300

    document, add plymouth and gdm3
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- install.sh
+++ install.sh
@@ -3,10 +3,25 @@
 set -e
 
 pushd /etc && git commit -asm "pre-inaes-hack-install commit"; popd
+
+# glib: some handy defaults
 cp glib-2.0/* /usr/share/glib-2.0/schemas/
 glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+# gdm3: greeter
+cp gdm3/* /etc/gdm3/
+/etc/init.d/gdm3 restart
+
+# applications: ugly hacks to get windows apps runing
 cp *.desktop /usr/share/applications/
 chmod +x *.sh
 cp hack*.sh /usr/bin/
 cp -r pam/*  /etc/
+
+# plymouth: boot theme
+cp -a plymouth/* /usr/share/plymouth/themes/
+plymouth-set-default-theme -R inaes
+perl -pi -e 'm/^\s*GRUB_CMDLINE_LINUX_DEFAULT.*splash.*\"/ || s/^(\s*GRUB_CMDLINE_LINUX_DEFAULT.*)\"\S*$/\1 splash\"/' /etc/default/grub
+update-grub
+
 pushd /etc && git commit -asm "post-inaes-hack-install commit"; popd
