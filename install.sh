793a4a257aa2c6c1dfafb9caaffdb34e87b70a61
root <root@terminal16>
2012-08-09 13:59:17 -0300

    init
    Signed-off-by: root <root@terminal16>

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+pushd /etc && git commit -asm "pre-inaes-hack-install commit"; popd
+cp glib-2.0/* /usr/share/glib-2.0/schemas/
+glib-compile-schemas /usr/share/glib-2.0/schemas/
+cp *.desktop /usr/share/applications/
+chmod +x *.sh
+cp hack*.sh /usr/bin/
+cp -r pam/*  /etc/
+pushd /etc && git commit -asm "post-inaes-hack-install commit"; popd
